e47547d692439c5e94131d705e321f1852258aac
Prin0516 <105021046@live.asia.edu.tw>
2016-11-21 16:00:43 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,31 @@
 ﻿package ex;
+
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
  * Author: 1050210XX 周永振老師
  */
+import java.util.Scanner;
 
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入");
+		int a = scn.nextInt();
+		int n = scn.nextInt();
+		fun(a, n);
 
+	}
+
+	public static void fun(int a, int n) {
+		String num = "　";
+		while (a != 0) {
+			num = a % n + num;
+			a = a / n;
+		}
+		System.out.println(num);
 	}
 
 }
