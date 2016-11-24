8f63ef191adcc509b50ca150a86e2078ba627a2f
Prin0516 <105021046@live.asia.edu.tw>
2016-11-25 00:05:24 +0800

    ex hm

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -12,20 +12,43 @@ public class ex02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		System.out.println("請輸入");
-		int a = scn.nextInt();
+
+		System.out.println("請輸入幾進制轉幾進制");
 		int n = scn.nextInt();
-		fun(a, n);
+		int s = scn.nextInt();
+		System.out.println("請輸入數字");
+		int a = scn.nextInt();
+		if (n == 10) {
+			System.out.println(fun(a, s));
+		} else if (s == 10) {
+			System.out.println(fun2(a, n));
+		} else {
+			System.out.println(fun(fun2(a, n), s));
+
+		}
 
 	}
 
-	public static void fun(int a, int n) {
+	public static String fun(int a, int s) {
 		String num = "　";
 		while (a != 0) {
-			num = a % n + num;
-			a = a / n;
+			num = a % s + num;
+			a = a / s;
 		}
-		System.out.println(num);
+		return num;
+	}
+
+	public static int fun2(int a, int n) {
+		int sum = 0;
+		int b = 0;
+		while (a != 0) {
+			sum = (int) (sum + (a % 10) * Math.pow(n, b));
+			b++;
+			a = a / 10;
+
+		}
+		return sum;
+
 	}
 
 }
--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -12,7 +12,50 @@ public class ex04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
+		com compu = new com();
+		System.out.println("請輸入");
 		int a = scn.nextInt();
+		char c = scn.next().charAt(0);
+		if (c == '+' || c == '-' || c == '*' || c == '/') {
+			int b = scn.nextInt();
+			System.out.println(compu.computer(a, b, c));
+		} else if (c == '^' || c == '√' || c == '3') {
+			System.out.println(compu.computer(a, 0, c));
+		}else{
+			System.out.println("錯誤");
+		}
+	}
+
+}
+
+class com {
+	public static float computer(int a, int b, char op) {
+		float num = 0;
+		switch (op) {
+		case '+':
+			num = a + b;
+			break;
+		case '-':
+			num = a - b;
+			break;
+		case '*':
+			num = a * b;
+			break;
+		case '/':
+			num = a / b;
+			break;
+		case '^':
+			num = (float) Math.pow(a, 2);
+			break;
+		case '3':
+			num = (float) Math.pow(a, 3);
+			break;
+		case '√':
+			num = (float) Math.sqrt(a);
+			break;
+
+		}
+		return num;
 	}
 
 }
